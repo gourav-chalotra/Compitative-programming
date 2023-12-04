259dff1e95d46b1d9d099397589f075cfee76cbf
Gourav Chalotra <90745717+gourav-chalotra@users.noreply.github.com>
2023-12-04 23:05:35 +0530

    level (EASY)

---
+++ leet code/string/daily problem/2264_Largest_3_Same_Digit_Number_in_String.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string ans = "";
+        for(int i=1; i<num.size()-1; i++) {
+            if(num[i-1] == num[i] && num[i] == num[i+1]) {
+				string temp = {num[i-1], num[i], num[i+1]};
+                ans = max(ans, temp);
+            }
+        }
+        return ans;    
+    }
+};
+int main()
+{
+    Solution s;
+    cout << s.largestGoodInteger("222");
+}
