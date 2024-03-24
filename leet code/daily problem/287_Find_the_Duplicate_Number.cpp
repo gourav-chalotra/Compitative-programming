4dcf963eacfd44ed390f235bc14576e6c53d22a8
gourav-chalotra <chalgourav82@gmail.com>
2024-03-24 07:48:48 +0530

    Medium

---
+++ leet code/daily problem/287_Find_the_Duplicate_Number.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i = 0;i < nums.size(); i++){
+            mp[nums[i]]++;
+            if(mp[nums[i]] > 1)
+                return nums[i];
+        }
+        return -1;
+    }
+};
+int main()
+{
+    
+}
