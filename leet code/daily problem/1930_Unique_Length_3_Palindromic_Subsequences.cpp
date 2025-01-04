fd0dbef4c2daec288c251b001dafebaa9ba739e5
gourav-chalotra <gouravchalotra4141@gmail.com>
2025-01-04 23:20:01 +0530

    Medium

---
+++ leet code/daily problem/1930_Unique_Length_3_Palindromic_Subsequences.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int countPalindromicSubsequence(string s)
+    {
+        int ans = 0;
+        unordered_set<char> letters;
+        for (char c : s)
+        {
+            letters.insert(c);
+        }
+        for (char letter : letters)
+        {
+            int i = -1;
+            int j = 0;
+            for (int k = 0; k < s.size(); k++)
+            {
+                if (s[k] == letter)
+                {
+                    if (i == -1)
+                    {
+                        i = k;
+                    }
+                    j = k;
+                }
+            }
+
+            unordered_set<char> between;
+            for (int k = i + 1; k < j; k++)
+            {
+                between.insert(s[k]);
+            }
+            ans += between.size();
+        }
+        return ans;
+    }
+};
+int main()
+{
+    Solution s;
+    string str = "aabca";
+    cout << s.countPalindromicSubsequence(str);
+}
