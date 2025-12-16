5fbb6998601be825eee0b3a75c8a451cfc40b9b7
Gourav-Chalotra <chalgourav82@gmail.com>
2025-12-16 20:58:23 +0530

    correcting

---
+++ leet code/daily problem/29_Divide_Two_Integers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int divide(int dividend, int divisor)
+    {
+        
+    }
+};
+
+int main()
+{
+    Solution s;
+    s.divide(10,3);
+}
