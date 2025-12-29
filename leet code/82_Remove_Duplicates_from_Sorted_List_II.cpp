36d30707f130deb698178702d4c1cfcb7d71f347
Gourav-Chalotra <chalgourav82@gmail.com>
2025-12-29 17:48:08 +0530

    Medium

---
+++ leet code/82_Remove_Duplicates_from_Sorted_List_II.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        if(!head){
+            return head;
+        }
+        ListNode*curr = head, *prev = nullptr, *next = nullptr;
+        while(curr)
+        {
+            next = curr->next;
+            if(next == nullptr)
+                break;
+            else if (next->val == curr->val)
+            {
+                if(curr == head)
+                {
+                    while(head != nullptr && head->val == next->val)
+                        head = head->next;
+                    curr = head;
+                    continue;
+                }
+                else
+                {
+                    while(next != nullptr && next->val == curr->val)
+                        next = next->next;
+                    curr = next;
+                    prev->next = next;
+                    continue;
+                }
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+        return head;
+    }
+};
+int main()
+{
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(1);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(2);
+    head->next->next->next->next = new ListNode(3);
+
+
+    Solution s;
+    ListNode*temp  = s.deleteDuplicates(head);
+    while(temp){
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+    
+}
